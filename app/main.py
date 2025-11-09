7352ccf10f7c3287c22f9461b1f43003be6762b7
“Harsh6575” <harsh.190410107146@gmail.com>
2025-11-09 12:42:44 +0530

    feat: removing deprecated on_event function with lifespan function

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.core.db import engine, Base, redis_client, mongo_client
 
-app = FastAPI(title="Shortify")
-
-@app.on_event("startup")
-async def startup():
-    # Create tables
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
     async with engine.begin() as conn:
-      await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
     print("✅ Databases initialized successfully.")
-
-@app.on_event("shutdown")
-async def shutdown():
+    
+    yield  # Application runs here
+    
+    # Shutdown
     await redis_client.close()
     mongo_client.close()
-    print("🛑 Databases connections closed.")
+    print("🛑 Database connections closed.")
+
+app = FastAPI(title="Shortify", lifespan=lifespan)
 
 @app.get("/")
 def root():
